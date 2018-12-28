9b4fd1072b4c978cbd2c0b2627c22c533d9ca0e7
Edwin Capel <edwinrc1026@gmail.com>
2018-12-28 16:53:27 +0800

    login works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component, Fragment } from 'react';
 import './App.scss';
+import axios from 'axios'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import {Route, Switch} from 'react-router-dom'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        current_user: {}
+    }
+  }
+
+  componentDidMount(){
+      axios({
+        method: 'get',
+        url: 'http://localhost:5000/api/v1/users/me',
+        headers: {
+          'authorization': `Bearer ${localStorage.jwt}`
+        }
+      })
+      .then(({data}) => {
+        this.setState({
+          current_user:data
+        })
+      })
+      .catch(error => {
+        console.log('ERROR: ', error); 
+      })
+  }
+
   render(){
     return (
       <Fragment>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -18,7 +18,6 @@ export default class SignUp extends Component {
         lastname:'',
         companyname:'',
         message:'',
-        loggedin:false,
         loading:false,
         hasError:false,
         errors:[],
@@ -45,7 +44,9 @@ export default class SignUp extends Component {
             const {data} = response;
             const {message, auth_token} = data
             sessionStorage.setItem('jwt',auth_token)
-            this.setState({loggedin:true,message:message})
+            this.setState({
+                message:message
+            })
         })
         .catch(catcherror => {
             const {response} = catcherror
@@ -104,94 +105,94 @@ export default class SignUp extends Component {
     }
 
     render(){
-        if(this.state.loggedin === true){
+        if(localStorage.getItem('jwt')){
             return <Redirect to= '/'/>
         }
         else{ 
-        return(
-            <section className="h-100" id="login-page">
-                <Container fluid className="h-100">
-                    <Row className="h-100">
-                        <Col md="7" className="h-100 d-none d-md-block" id="login-left-banner" style={leftBg}>
-                            <div className="mb-auto mt-3">
-                                <h3>LOGO</h3>
-                            </div>
-                        </Col>
-                        <Col md="5" className="h-100 d-flex align-items-start flex-column" id="login-right-banner">
-                            <div className="mb-auto m-5">
-                                <h1>Sign Up</h1>
-                                <p>Hello! Let's get started by making your account!</p>
-                            </div>
-                            <Form className="mb-auto w-100 p-5" onSubmit={this.handleSubmit && this.postCredentials}>
-                                <FormGroup>
-                                    <Input onInput={this.handleFirstName}
-                                        className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
-                                        name = "firstname" 
-                                        placeholder ="First Name"
-                                        required
-                                    />
-                                    <Input onChange={this.handleLastName}
-                                        className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
-                                        name = "lastname" 
-                                        placeholder ="Last Name"
-                                        required
-                                    />
-                                    <Input onChange={this.handleEmail}
-                                        className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
-                                        name = "email" 
-                                        type='email'
-                                        placeholder ="Email"
-                                        required
-                                    />
-                                    <Input onChange={this.handleCompanyName}
-                                        className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
-                                        name = "companyname" 
-                                        placeholder ="Company Name"
-                                        required
-                                    />
-                                    <Input onChange = {this.handlePassword}
-                                        type='password'
-                                        className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
-                                        name = "password" 
-                                        placeholder ="Password"
-                                        required
-                                    />
-                                    <div className="d-flex flex-row mt-3 ml-1">
-                                        <Button className="btn btn-dark" value="Login">
-                                            Sign Up
-                                        </Button>
-                                        
-                                        <a 
-                                        href="#" 
-                                        className="btn btn-primary h-50 ml-1" id="google-button"
-                                        >
-                                            <FontAwesomeIcon className="fab fa-google" icon={faVrCardboard}/>
-                                        </a>
-                                    </div>
-                                </FormGroup>
-                            </Form>
-                            <div>
-                                <h6>
-                                {this.state.hasError === true ? this.state.errors.map((errors,index) =>
-                                <li key = {index}>{errors}</li>) 
-                                : this.state.message}
-                                </h6>
-                            </div>
-                            <div className="mt-auto mx-auto mb-3">
-                                <small className="text-muted">
-                                    Already have an account? Login &nbsp;
-                                    <Link to={'/login'} className="text-muted border-bottom">
-                                        here!
-                                    </Link> 
-                                </small>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        )
-                                }
-    }
+            return(
+                <section className="h-100" id="login-page">
+                    <Container fluid className="h-100">
+                        <Row className="h-100">
+                            <Col md="7" className="h-100 d-none d-md-block" id="login-left-banner" style={leftBg}>
+                                <div className="mb-auto mt-3">
+                                    <h3>LOGO</h3>
+                                </div>
+                            </Col>
+                            <Col md="5" className="h-100 d-flex align-items-start flex-column" id="login-right-banner">
+                                <div className="mb-auto m-5">
+                                    <h1>Sign Up</h1>
+                                    <p>Hello! Let's get started by making your account!</p>
+                                </div>
+                                <Form className="mb-auto w-100 p-5" onSubmit={this.handleSubmit && this.postCredentials}>
+                                    <FormGroup>
+                                        <Input onInput={this.handleFirstName}
+                                            className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                            name = "firstname" 
+                                            placeholder ="First Name"
+                                            required
+                                        />
+                                        <Input onChange={this.handleLastName}
+                                            className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                            name = "lastname" 
+                                            placeholder ="Last Name"
+                                            required
+                                        />
+                                        <Input onChange={this.handleEmail}
+                                            className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                            name = "email" 
+                                            type='email'
+                                            placeholder ="Email"
+                                            required
+                                        />
+                                        <Input onChange={this.handleCompanyName}
+                                            className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                            name = "companyname" 
+                                            placeholder ="Company Name"
+                                            required
+                                        />
+                                        <Input onChange = {this.handlePassword}
+                                            type='password'
+                                            className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                            name = "password" 
+                                            placeholder ="Password"
+                                            required
+                                        />
+                                        <div className="d-flex flex-row mt-3 ml-1">
+                                            <Button className="btn btn-dark" value="Login">
+                                                Sign Up
+                                            </Button>
+                                            
+                                            <a 
+                                            href="#" 
+                                            className="btn btn-primary h-50 ml-1" id="google-button"
+                                            >
+                                                <FontAwesomeIcon className="fab fa-google" icon={faVrCardboard}/>
+                                            </a>
+                                        </div>
+                                    </FormGroup>
+                                </Form>
+                                <div>
+                                    <h6>
+                                    {this.state.hasError === true ? this.state.errors.map((errors,index) =>
+                                    <li key = {index}>{errors}</li>) 
+                                    : this.state.message}
+                                    </h6>
+                                </div>
+                                <div className="mt-auto mx-auto mb-3">
+                                    <small className="text-muted">
+                                        Already have an account? Login &nbsp;
+                                        <Link to={'/login'} className="text-muted border-bottom">
+                                            here!
+                                        </Link> 
+                                    </small>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                )
+            }
+        }
     }
 
 
