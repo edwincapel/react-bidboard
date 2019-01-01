60866b4ac66f1c88498a78eef54e1e5efcc4eaae
Edwin Capel <edwinrc1026@gmail.com>
2019-01-01 20:37:33 +0800

    upload axios wip

--- src/components/DropFile.js
+++ src/components/DropFile.js
@@ -1,45 +1,143 @@
-import React,{Component,Fragment} from 'react'
+import React,{Component} from 'react'
 import Dropzone from 'react-dropzone'
-import {Button} from 'reactstrap'
+import axios from 'axios'
+import {Button,Form,FormGroup,Input,Table} from 'reactstrap'
+
 
 export default class DropFile extends Component {
     constructor() {
         super()
         this.state = {
-          file: []
+            description: "",
+            campaign_name: "",
+            file: [],
+            imgSrc: null,
+            fileName: "",
+            fileSize: "",
+            currentUser: JSON.parse(localStorage.getItem('currentUser'))
         }
       }
+
+    handleDescriptionChange = (e) => {
+        this.setState({description: e.target.value})
+    }
+
+    handleCampaignNameChange = (e) => {
+        this.setState({campaign_name: e.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        axios({
+            method: 'post',
+            url: 'http://127.0.0.1:5000/api/v1/media/upload',
+            headers: {
+                'content-type': 'multipart/form-data',
+                'authorization': `Bearer ${localStorage.jwt}`
+            },
+            data: {
+                "campaign_name": this.state.campaign_name,
+                "description": this.state.description,
+                "user_media": this.state.file[0],
+                'user_id': this.state.currentUser.id
+            }
+          })
+        .then( response => {
+            // console.log(response)
+            console.log(response);
+            
+        })
+        .catch(error => {
+            console.log(error);
+            
+        }); 
+    }
     
-      onDrop(file) {
-        this.setState({file});
-      }
+    onDrop(file) {
+        file.map(file => (
+            this.setState({
+                fileName: file.name,
+                fileSize: (parseInt(file.size)/(Math.pow(1024,2))).toFixed(2)
+            })
+        ))
+
+        const reader = new FileReader()
+        reader.addEventListener("load",() => {
+            console.log(reader.result);
+            this.setState({
+                file,
+                imgSrc: reader.result
+            })
+        })
+        reader.readAsDataURL(file[0])
+
+    }
     
-      onCancel() {
+      onCancel = () => {
         this.setState({
-          file: []
+          file: [],
+          imgSrc: null
         });
       }
 
     render() {
-        const file = this.state.file.map(file => (
-            <li key={file.name}>
-              {file.name} - {file.size} bytes
-            </li>
-          ))
-
-        console.log(file[0]);
+        const {imgSrc,fileName,fileSize} = this.state
         
         if (this.state.file.length > 0) {
             return(
-                <Fragment>
-                    <h1>File Preview here with description input here</h1>
-                    <ul>{file}</ul>
-                </Fragment>
+                <div id="preview" className="w-100">
+                    {imgSrc !== null ? <img src={imgSrc} className="w-100" height="244px" alt=""/> : ''}
+
+                    <Table className="w-50 mt-5 border ml-5">
+                        <thead>
+                        <tr>
+                            <th className="border-right">#</th>
+                            <th className="border-right">File Name</th>
+                            <th className="border-right">File Size</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row" className="border-right">1</th>
+                            <td className="border-right">{fileName}</td>
+                            <td className="border-right">{fileSize} mb</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+
+                    <Form className="w-100 p-5" onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Input 
+                                className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                name = "campaign_name" 
+                                placeholder ="Campaign Name"
+                                value={this.state.campaign_name}
+                                onChange={this.handleCampaignNameChange}
+                            />
+                            <Input 
+                                className = "form-control border-top-0 border-left-0 border-right-0 bg-transparent" 
+                                name = "description" 
+                                placeholder ="description"
+                                value={this.state.description}
+                                onChange={this.handleDescriptionChange}
+                            />
+                            <div className="d-flex flex-row mt-3 ml-1">
+                                <Button className="btn btn-dark" value="Submit">
+                                    Sumbit
+                                </Button>
+                                <Button className="btn btn-danger ml-1" onClick={this.onCancel}>
+                                    Cancel
+                                </Button>
+                            </div>
+                        </FormGroup>
+                    </Form>
+                </div>
+
             )
         }
         else{
             return (
-                <section id="dropzone" className="border w-100 h-100">
+                <section id="dropzone" className="w-100 h-100">
                     <Dropzone
                     onDrop={this.onDrop.bind(this)}
                     onFileDialogCancel={this.onCancel.bind(this)}
@@ -60,7 +158,6 @@ export default class DropFile extends Component {
               </section>
             )
         }
-
     }
 }
 
